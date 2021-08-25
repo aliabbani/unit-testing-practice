d6d625400df5a7318d71712f21fa58145a9fddc2
ali abbani <aliox@ahlam.com>
2021-08-25 21:17:15 +0300

    Edit task 1

--- src/task1.js
+++ src/task1.js
@@ -3,7 +3,7 @@ function stringLength(str) {
   if ((l > 0 && l < 11)) {
     return str.length;
   }
-  throw new Error('The string should be at least 1 character long and not more than 10 characters');
+  return false;
 }
 
 module.exports = stringLength;
--- src/task1.test.js
+++ src/task1.test.js
@@ -4,10 +4,6 @@ test('When type a string, should return string length', () => {
   expect(stringLength('ali')).toBe(3);
 });
 
-test('When type a string, should return string length', () => {
-  expect(stringLength('type')).toBe(4);
-});
-
 test('When type a string, the string should be between 1 and 10 . else should return invalid', () => {
-  expect(stringLength('sophisticated')).toThrowError('invalid');
+  expect(stringLength('sophisticated')).toBeFalsy();
 });
