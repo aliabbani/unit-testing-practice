ebcc1cf0368e2cd4267257fec1bd7788f4ad72e0
ali abbani <aliox@ahlam.com>
2021-08-25 17:02:25 +0300

    Add task1

--- src/order-total.js
+++ src/order-total.js
@@ -1,5 +1,10 @@
-function orderTotal(order) {
-  return order.items.reduce((prev, cur) => cur.price * (cur.quantity || 1) + prev, 0);
+function stringLength(str) {
+  const l = str.length;
+  if ((l > 0 && l < 11)) {
+    return str.length;
+  }
+  throw new Error('The string should be at least 1 character long and not more than 10 characters');
 }
-
-module.exports = orderTotal;
+// expand your function to make it check if the string is at least 1 character long
+// and not longer than 10 characters. Throw errors if those conditions are not met.
+module.exports = stringLength;
--- src/order-total.test.js
+++ src/order-total.test.js
@@ -1,26 +1,13 @@
-const orderTotal = require('./order-total');
+const stringLength = require('./order-total.js');
 
-it('Quantity', () => expect(orderTotal({
-  items: [
-    { name: 'Dragon candy', price: 2, quantity: 3 },
-  ],
-})).toBe(6));
+test('When type a string, should return string length', () => {
+  expect(stringLength('ali')).toBe(3);
+});
 
-it('No quantity specified', () => expect(orderTotal({
-  items: [
-    { name: 'Dragon candy', price: 3 },
-  ],
-})).toBe(3));
+test('When type a string, should return string length', () => {
+  expect(stringLength('type')).toBe(4);
+});
 
-it('Happy path (Example 1)', () => expect(orderTotal({
-  items: [
-    { name: 'Dragon food', price: 8, quantity: 1 },
-    { name: 'Dragon cage (small)', price: 800, quantity: 1 },
-  ],
-})).toBe(808));
-it('Happy path (Example 2)', () => expect(orderTotal({
-  items: [
-    { name: 'Dragon collar', price: 20, quantity: 1 },
-    { name: 'Dragon chew toy', price: 40, quantity: 1 },
-  ],
-})).toBe(60));
+test('When type a string, the string should be between 1 and 10 . else should return invalid', () => {
+  expect(stringLength('sophisticated')).toThrowError('invalid');
+});
